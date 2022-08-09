f2cfb15376003769680d6339a9cb8d20018e66c4
Carlos-Simim <carlinhossimim@hotmail.com>
2022-08-09 16:20:35 -0300

    Initial commit

---
+++ MorseCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Insira a frase que você deseja traduzir para morse: ");
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+            char[] translation = input.ToCharArray();
+            string output = "";
+
+            foreach(char c in translation)
+            {
+                output = output + Dictionary._textToMorse[c] + " ";                
+            }
+
+            Console.WriteLine("Código morse: " + output);
+
+            Console.ReadKey();
+        }
+    }
+}
